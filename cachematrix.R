a7a40d78dece710ac4e90949d2ac390dd562a0dc
praveenkumar6 <praveenkumarmymail@gmail.com>
2015-01-24 00:53:48 +0530

    programming assignment 2 - first commit
    Added the body for the functions makeCacheMatrix and cacheSolve.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## accepts input matrix and caches its inverse if it is already computed else assigns it to null
 
 makeCacheMatrix <- function(x = matrix()) {
+##assign inverse to null initially
+ inverse <- NULL
+
+        set <- function(y) {
+                x <<- y
+                inverse <<- NULL
+        }
+
+        get <- function() x
+
+        setinverse <- function(inv) inverse <<- inv
+
+        getinverse <- function() inverse
+
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## accepts the output of the function makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+        inverse <- x$getinverse()
+
+        if(!is.null(inverse)) {
+                message("getting cached data")
+                return(inverse)
+        }
+
+        data <- x$get()
+
+        inv <- solve(data, ...)
+
+        x$setinverse(inv)
+
+        inv
 }
